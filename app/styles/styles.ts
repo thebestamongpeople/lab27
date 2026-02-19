e8a4cbb084e88319e835421ec95e1e986b23dbc8
thebestamongpeople <storozhev2004@bk.ru>
2026-02-19 12:37:55 +0300

    Change background color

--- app/styles/styles.ts
+++ app/styles/styles.ts
@@ -5,7 +5,7 @@ export const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff",
+    backgroundColor: "#000000",
   },
   modeText: {
     fontSize: 32,
